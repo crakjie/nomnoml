47662922756e573633e631455967d97db77fd3b4
etienne <waa@hotmail.fr>
2014-08-01 14:22:34 +0200

    First refactor to fetch json and not string

--- nomnoml.parser.js
+++ nomnoml.parser.js
@@ -13,7 +13,7 @@ nomnoml.parse = function (source){
 		return [tokens[0].trim(), tokens[1].trim()]
 	}))
 	var pureDiagramCode = _.map(lines, onlyCompilables).join('\n').trim()
-	var ast = nomnoml.transformParseIntoSyntaxTree(nomnoml.intermediateParse(pureDiagramCode))
+	var ast = nomnoml.transformParseIntoSyntaxTree()
 	ast.directives = directives
 	return ast
 }
@@ -22,52 +22,39 @@ nomnoml.intermediateParse = function (x){
 	return parser.parse(x)
 }
 
-nomnoml.transformParseIntoSyntaxTree = function (entity){
+nomnoml.transformParseIntoSyntaxTree = function (){
 
 	var relationId = 0
 
-	function transformCompartment(parts){
-		var lines = []
-		var rawClassifiers = []
-		var relations = []
-		_.each(parts, function (p){
-			if (typeof p === 'string')
-				lines.push(p)
-			if (p.assoc){ // is a relation
-				rawClassifiers.push(p.start)
-				rawClassifiers.push(p.end)
-				relations.push({
-                    id: relationId++,
-                    assoc: p.assoc,
-                    start: p.start.parts[0][0],
-                    end: p.end.parts[0][0],
-                    startLabel: p.startLabel,
-                    endLabel: p.endLabel
-                })
-            }
-			if (p.parts){ // is a classifier
-				rawClassifiers.push(p)
-            }
-		})
-		var allClassifiers = _.map(rawClassifiers, transformItem)
-		var noDuplicates = _.map(_.groupBy(allClassifiers, 'name'), function (cList){
-			return _.max(cList, function (c){ return c.compartments.length })
-		})
-
-		return nomnoml.Compartment(lines, noDuplicates, relations)
+	function transformRelations(adjacency){
+		return  {
+            id: 0,
+            assoc: '-',
+            start: entity.name,
+            end: adjacency.end,
+            startLabel: adjacency.name,
+            endLabel: ""
+        }
 	}
 
-	function transformItem(entity){
-		if (typeof entity === 'string')
-			return entity
-		if (_.isArray(entity))
-			return transformCompartment(entity)
-		if (entity.parts){
-			var compartments = _.map(entity.parts, transformCompartment)
-			return nomnoml.Classifier(entity.type, entity.id, compartments)
-		}
-		return undefined
+	function transformAdjacencies(entity){
+		_.map(entity.adjacencies, transformRelations)
+	}
+    
+	function transformProperties(propertie){
+		return propertie.name + " : " +propertie.returnType
 	}
 
-	return transformItem(entity)
+	function transformClassifier(entity){
+		var compartmentProperties = nomnoml.Compartment(_.map(entity.properties, transformCompartment), [], [])
+		var compartmentName = nomnoml.Compartment([entity.name], [], [])		
+		return nomnoml.Classifier("CLASS", entity.name, concat(compartmentName,compartmentProperties))		
+	}
+
+	return $.getJSON('http://localhost:9002/schema',function(data){
+		var classifiers = _.map(data,transformClassifier)
+		var adjacencies = _.map(data,transformAdjacencies)
+		nomnoml.Compartment([entity.name], classifiers, adjacencies)
+	})
+
 }
